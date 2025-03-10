a1145245bc2d09ce2035886106b0b0e40dbb2aa4
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-03-10 21:19:03 +0200

    init backend

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Allows parsing of JSON requests
+
+// Serve the static files from the React app
+
+// API endpoint
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from the server!" });
+});
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// Handle requests by serving index.html for all routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log("Server is running on port " + PORT);
+});
