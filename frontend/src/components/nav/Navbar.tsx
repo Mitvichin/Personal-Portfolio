254d8caad04dd9876abc1e70423afb65e24031f0
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-03-20 22:29:55 +0200

    adding styles for nav links

--- frontend/src/components/nav/Navbar.tsx
+++ frontend/src/components/nav/Navbar.tsx
@@ -1,26 +1,39 @@
-import { NavLink } from "react-router";
+import { NavLink, NavLinkRenderProps } from "react-router";
 import { routes } from "../../router";
 
 export const Navbar: React.FC = () => {
+  const getClassName = ({ isActive }: NavLinkRenderProps) =>
+    `flex-1 flex group hover:cursor-pointer transition-all duration-500 ${
+      isActive ? "bg-blue-100" : ""
+    }`;
   return (
-    <div className="h-[30px] flex w-full text-center align-middle shadow-sm rounded-2xl border border-gray-300">
+    <div className="h-[30px] flex w-full text-center overflow-hidden align-middle shadow-sm rounded-2xl border border-gray-300">
       <NavLink
         to={routes.experience}
-        className="flex-1 flex text-base border-r hover:scale-110 hover:cursor-pointer transition-all border-gray-300"
+        className={(data) => {
+          const className = getClassName(data);
+          return `${className} border-r border-gray-300`;
+        }}
       >
-        <p className="m-auto">Experience</p>
+        <p className="m-auto group-hover:scale-110 transition-all">
+          Experience
+        </p>
       </NavLink>
       <NavLink
         to={routes.sideProject}
-        className="flex-1 flex border-r hover:scale-110 hover:cursor-pointer transition-all border-gray-300"
+        className={(data) => {
+          const className = getClassName(data);
+          return `${className} border-r border-gray-300`;
+        }}
       >
-        <p className="m-auto">Side Projects</p>
+        <p className="m-auto group-hover:scale-110 transition-all">
+          Side Projects
+        </p>
       </NavLink>
-      <NavLink
-        to={routes.contactMe}
-        className="flex-1 flex hover:scale-110 hover:cursor-pointer transition-all"
-      >
-        <p className="m-auto">Contact Me</p>
+      <NavLink to={routes.contactMe} className={getClassName}>
+        <p className="m-auto group-hover:scale-110 transition-all">
+          Contact Me
+        </p>
       </NavLink>
     </div>
   );
