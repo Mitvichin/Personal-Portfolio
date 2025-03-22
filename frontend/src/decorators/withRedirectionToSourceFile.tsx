f4dd2e1fc69b117740842aca0e5b2f24b980835e
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-03-22 20:16:09 +0200

    added vite plugin to conserve relative paths, minor refactors

--- frontend/src/decorators/withRedirectionToSourceFile.tsx
+++ frontend/src/decorators/withRedirectionToSourceFile.tsx
@@ -15,33 +15,34 @@ export const withRedirectionToSourceFiles = <
       const target = e.target as HTMLElement;
       const wordSelection = window.getSelection();
       const searchString = wordSelection?.toString().trim() || "";
-
-      target.id = "temp-search-id";
       const parentIndex = e.currentTarget.innerHTML.indexOf(target.id);
       const sentanceIndexStart =
         e.currentTarget.innerHTML.indexOf(">", parentIndex) + 1;
-
       const wordSelectionOffset = wordSelection?.anchorOffset || 0;
       const targetWordIndex = sentanceIndexStart + wordSelectionOffset;
 
+      target.id = "temp-search-id";
       let currentIndex = -1;
-      const indexes = [];
+      const searchWordIndexes = [];
 
+      // Find index of correct searchString in case of multiple present
       while (
         (currentIndex = e.currentTarget.innerHTML.indexOf(
           searchString,
           currentIndex + 1
         )) !== -1
       ) {
-        indexes.push(currentIndex);
+        searchWordIndexes.push(currentIndex);
         if (currentIndex === targetWordIndex) break;
       }
 
       target.id = "";
 
-      const wordCount = indexes.length ? indexes.length : -1;
+      // Find which occurence of the word has been clicked
+      const wordCount = searchWordIndexes.length
+        ? searchWordIndexes.length
+        : -1;
 
-      console.log(fileName);
       try {
         const { content: fileContent, url } = await downloadRepoFile(
           searchString,
@@ -51,6 +52,7 @@ export const withRedirectionToSourceFiles = <
         const jsxOffset = fileContent.search("<.*>");
         let i = jsxOffset - searchString.length;
         let counter = 0;
+
         while (
           (i = fileContent.indexOf(searchString, i + searchString.length)) >= 0
         ) {
@@ -64,14 +66,15 @@ export const withRedirectionToSourceFiles = <
 
         if (targetIndexInSourceFile === -1) throw Error("404 Word not found");
 
-        const indexes = [];
+        let newLinesCount = 0;
         i = -1;
+
         while ((i = fileContent.indexOf("\n", i + 1)) >= 0) {
           if (i >= targetIndexInSourceFile) break;
-          indexes.push(i);
+          newLinesCount++;
         }
 
-        const lineInSource = indexes.length + 1;
+        const lineInSource = newLinesCount + 1;
 
         window.open(`${url}#L${lineInSource}`, `_newtab-#L${lineInSource}`);
       } catch {
