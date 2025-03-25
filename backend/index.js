e62cf53b0227de844b78e935688eca093a0cef74
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-03-25 17:55:40 +0200

    implemented redis rate limitting

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const messageRouter = require("./routes/Message.route.js");
 const gridRouter = require("./routes/Grid.route.js");
+const rateLimitMiddleware = require("./middlewares/rate-limit.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json()); // Allows parsing of JSON requests
+app.use('/api',rateLimitMiddleware) // Rate limitting
 
 // API endpoint
 app.use("/api/message", messageRouter);
