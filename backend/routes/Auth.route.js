abcdb680aaeb762d627217fb0c3b3c87537c0bc2
Ilia Mitvichin <i.mitvichin@tinqin.com>
2025-04-17 20:18:21 +0300

    testing NOV_ENV

--- backend/routes/Auth.route.js
+++ backend/routes/Auth.route.js
@@ -8,5 +8,8 @@ router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/verify-authentication", authController.verifyAuthentication);
 router.get("/csrf-token", authController.getCSRF);
+router.get("/env", (req, res) =>
+  res.status(200).json({ env: process.env.NODE_ENV })
+);
 
 module.exports = router;
